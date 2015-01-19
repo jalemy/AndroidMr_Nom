73ca90e30fc49903938f003e2d535ca38a20fa4a
jalemy <jalemy.xps@gmail.com>
2015-01-19 12:14:01 +0900

    Screen実装途中

---
+++ src/com/androidgames/mrnom/MrNomGame.java
@@ -0,0 +1,13 @@
+package com.androidgames.mrnom;
+
+import com.androidgames.framework.Screen;
+import com.androidgames.framework.impl.AndroidGame;
+
+public class MrNomGame extends AndroidGame {
+
+	@Override
+	public Screen getStartScreen() {
+		return new LoadingScreen(this);
+	}
+
+}
